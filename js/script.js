a111d2e70f79117751abb15818ac79f71c06f53b
Bryan <mgbryan90@hotmail.com>
2022-08-18 19:24:29 -0500

    complete loop for typed

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 // Typing Animation
 let typed = new Typed(".typing", {
-    strings: ["","Full-Stack Developer", "Computer Engineer", ""],
+    strings: ["Full-Stack Developer", "Computer Engineer"],
     typeSpeed:100,
     BackSpeed:60,
+    fadeOut: true,
     loop:true
 })
